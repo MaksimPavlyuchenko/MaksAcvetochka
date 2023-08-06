02842da52969fdcd4b3bf3c88435582e30479eb1
MaksimPavlyuchenko <maksimpavlyuchenko82@gmail.com>
2023-08-06 11:38:25 +0300

    Maks-tasks

--- src/redux/commentApi.js
+++ src/redux/commentApi.js
@@ -1,15 +1,28 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_ENDPOINT = '/comments';
-const BASE_URL = '';
+const API_ENDPOINT = "/comments";
+const BASE_URL = "https://64c8d31ba1fe0128fbd648c9.mockapi.io/contacts";
 
 export const commentApi = createApi({
-  reducerPath: 'comments',
+  reducerPath: "comments",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
   }),
-  tagTypes: ['Comments'],
-  endpoints: (builder) => ({}),
+  tagTypes: ["Comments"],
+  endpoints: (builder) => ({
+    getComments: builder.query({
+      query: () => API_ENDPOINT,
+      providesTags: ["Comments"],
+    }),
+    addComments: builder.mutation({
+      query: (comment) => ({
+        url: API_ENDPOINT,
+        method: "POST",
+        body: comment,
+      }),
+      invalidatesTags: ["Comments"],
+    }),
+  }),
 });
 
-export const {} = commentApi;
+export const { useGetCommentsQuery, useAddCommentsMutation } = commentApi;
